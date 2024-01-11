3c0dce3cc258e61b50471f5bf396a085b8a83cf2
praveen Sahu <praveen.pkyt327867@gmail.com>
2024-01-11 17:31:42 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { TodoProvider } from './contexts';
 import { useEffect } from 'react';
+import { TodoForm, TodoItem } from './components';
 
 function App() {
     const [todos, setTodos] = useState([]);
@@ -43,7 +44,27 @@ function App() {
     return (
         <TodoProvider
             value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
-        ></TodoProvider>
+        >
+            <div className="bg-[#161A30] min-h-screen py-8">
+                <div className="w-full bg-[#31304D] max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+                        Manage Your Todos
+                    </h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */}
+                        <TodoForm />
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) => (
+                            <div key={todo.id} className="w-full">
+                                <TodoItem todo={todo} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </TodoProvider>
     );
 }
 
